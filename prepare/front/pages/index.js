b559de03c80ac4b65b189a3422f8d40e2f39ba3d
Minseok Chae <117037249+chaeminseok@users.noreply.github.com>
2023-06-25 23:56:23 +0900

    setting_06_25

--- prepare/front/pages/index.js
+++ prepare/front/pages/index.js
@@ -1,8 +1,9 @@
+import AppLayout from "../components/AppLayout";
 const Home = () => {
   return (
-    <>
+    <AppLayout>
       <div>Hello, Next!</div>
-    </>
+    </AppLayout>
   );
 };
 export default Home;
