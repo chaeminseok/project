4b8b659a99de7b5a8c94cc3668311fbdaf9fbf6b
Minseok Chae <117037249+chaeminseok@users.noreply.github.com>
2023-07-07 22:11:02 +0900

    07_07

--- prepare/front/components/PostImages.js
+++ prepare/front/components/PostImages.js
@@ -1,16 +1,43 @@
 import propTypes from "prop-types";
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 const PostImages = ({ images }) => {
   const [showImagesZoom, setShowImagesZoom] = useState(false);
+  const onZoom = useCallback(() => {
+    setShowImagesZoom(true);
+  });
   if (images.length === 1) {
     return (
       <>
-        <img src={images[0].src} alt={images[0].src} onClick={onZoom} />
+        <img
+          role="presentation"
+          width="50%"
+          src={images[0].src}
+          alt={images[0].src}
+          onClick={onZoom}
+        />
+        }
+        <img
+          role="presentation"
+          width="50%"
+          src={images[0].src}
+          alt={images[0].src}
+          onClick={onZoom}
+        />
       </>
     );
-  }
+  
 
-  return <div></div>;
+  return (
+    <div>
+      <img
+        role="presentation"
+        width="50%"
+        src={images[0].src}
+        alt={images[0].src}
+        onClick={onZoom}
+      />
+    </div>
+  );
 };
 
 PostImages.propTypes = {
