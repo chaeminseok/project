da9c2e9e7e2f743fddbf73ae7d42b6b9dae77099
Minseok Chae <117037249+chaeminseok@users.noreply.github.com>
2023-07-10 23:58:56 +0900

    07_10

--- prepare/front/components/ImagesZoom/index.js
+++ prepare/front/components/ImagesZoom/index.js
@@ -1,6 +1,36 @@
-const ImagesZoom = ({ images, onClose }) => {};
+import Slick from "react-slick";
+import PropTypes from "prop-types";
+const ImagesZoom = ({ images, onClose }) => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  return (
+    <div>
+      <header>
+        <h1>상세 이미지</h1>
+        <button onClick={onClose}>X</button>
+      </header>
+      <div>
+        <div>
+          <Slick
+            initialSlide={0}
+            afterChange={(slide) => setCurrentSlide(slide)}
+            infinite
+            arrows={false}
+            slidesToShow={1}
+            slidesToScroll={1}
+          >
+            {images.map((v) => (
+              <div key={v.src}>
+                <img src={v.src} alt={v.src} />
+              </div>
+            ))}
+          </Slick>
+        </div>
+      </div>
+    </div>
+  );
+};
 ImagesZoom.propTypes = {
-  images: propTypes.arrayOf(propTypes.objecy).isRequired,
-  onClose: propTypes.func.isRequired,
+  images: PropTypes.arrayOf(propTypes.objecy).isRequired,
+  onClose: PropTypes.func.isRequired,
 };
 export default ImagesZoom;
